b080997c6348f42ce3cd65dab34c817c854e4a63
ALYSSA NICOLE JANSSEN <52120159+AlyssaJanssen@users.noreply.github.com>
2023-04-24 18:40:05 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -69,16 +69,10 @@ async function deleteNote({ id, name }) {
   });
 }
 
-  return (
-<View className="App">
+ return (
+    <View className="App">
       <Heading level={1}>My Notes App</Heading>
       <View as="form" margin="3rem 0" onSubmit={createNote}>
-        <View
-  name="image"
-  as="input"
-  type="file"
-  style={{ alignSelf: "end" }}
-/>
         <Flex direction="row" justifyContent="center">
           <TextField
             name="name"
@@ -103,29 +97,22 @@ async function deleteNote({ id, name }) {
       </View>
       <Heading level={2}>Current Notes</Heading>
       <View margin="3rem 0">
-   {notes.map((note) => (
-  <Flex
-    key={note.id || note.name}
-    direction="row"
-    justifyContent="center"
-    alignItems="center"
-  >
-    <Text as="strong" fontWeight={700}>
-      {note.name}
-    </Text>
-    <Text as="span">{note.description}</Text>
-    {note.image && (
-      <Image
-        src={note.image}
-        alt={`visual aid for ${notes.name}`}
-        style={{ width: 400 }}
-      />
-    )}
-    <Button variation="link" onClick={() => deleteNote(note)}>
-      Delete note
-    </Button>
-  </Flex>
-))}
+        {notes.map((note) => (
+          <Flex
+            key={note.id || note.name}
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Text as="strong" fontWeight={700}>
+              {note.name}
+            </Text>
+            <Text as="span">{note.description}</Text>
+            <Button variation="link" onClick={() => deleteNote(note)}>
+              Delete note
+            </Button>
+          </Flex>
+        ))}
       </View>
       <Button onClick={signOut}>Sign Out</Button>
     </View>
